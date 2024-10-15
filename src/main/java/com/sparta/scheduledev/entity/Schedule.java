32d8af15d4330ae153fe950bda204136ef98cfe4
Younghan Yonn <dudgks1172@naver.com>
2024-10-15 14:41:32 +0900

    Entity 및 Test 구현

--- src/main/java/com/sparta/scheduledev/entity/Schedule.java
+++ src/main/java/com/sparta/scheduledev/entity/Schedule.java
@@ -1,18 +1,33 @@
 package com.sparta.scheduledev.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "schedule")
+
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // nullable :  null 허용 여부
+    // unique : 중복 허용 여부(false일 때 중복 허용)
+
+    //
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
+
+    // length에 몇으로 넣어야 할지 질문해보자
+    @Column(name = "password", nullable = false, length = 20)
     private String password;
+
+    @Column(name = "title", nullable = false, length = 500)
     private String title;
+
+    @Column(name = "contents", nullable = false, length = 500)
     private String contents;
+
     // 수정일
     // 등록일
 }
