2a781c9834281ab300286f0fa8d6e104b8e3a951
Younghan Yonn <dudgks1172@naver.com>
2024-10-15 17:08:24 +0900

    제어의 역전 적용

--- src/main/java/com/sparta/scheduledev/controller/ScheduleController.java
+++ src/main/java/com/sparta/scheduledev/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package com.sparta.scheduledev.controller;
 
 import com.sparta.scheduledev.dto.ScheduleRequestDto;
 import com.sparta.scheduledev.dto.ScheduleResponseDto;
+import com.sparta.scheduledev.entity.Schedule;
 import com.sparta.scheduledev.service.ScheduleService;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +12,23 @@ import java.util.List;
 @RequestMapping("/api/")
 public class ScheduleController {
 
+    private final ScheduleService scheduleService;
+
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+
     // 일정 조회
     @GetMapping("/schedule")
     public List<ScheduleResponseDto> getSchedule() {
-        ScheduleService scheduleService = new ScheduleService();
+        return scheduleService.getSchedule();
+
     }
 
     // 일정 등록
     @PostMapping("/schedule")
     public ScheduleResponseDto creatSchedule(ScheduleRequestDto requestDto) {
-        ScheduleService scheduleService = new ScheduleService(); // 매개변수 추가
         return scheduleService.createSchedule(requestDto);
     }
 
@@ -28,14 +36,12 @@ public class ScheduleController {
     // 일정 수정
     @PutMapping("/schedule/{id}")
     public Long updateSchedule(@PathVariable Long id, @RequestBody ScheduleRequestDto requestDto) {
-        ScheduleService scheduleService = new ScheduleService(); // 매개변수 추가
         return scheduleService.updateSchedule(id, requestDto);
     }
 
     // 일정 삭제
     @DeleteMapping("/schedule/{id}")
     public Long deleteSchedule(@PathVariable Long id){
-        ScheduleService scheduleService = new ScheduleService(); // 매개변수 추가
         return scheduleService.deleteSchedule(id);
     }
 }
